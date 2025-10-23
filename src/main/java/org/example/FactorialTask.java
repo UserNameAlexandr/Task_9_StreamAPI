2c8246f1d29b47abb6759b9c068bfc2cc1fabbc3
Alexandr Rozum <rozum_1998@mail.ru>
2025-10-23 15:02:18 +0300

    master

---
+++ src/main/java/org/example/FactorialTask.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.concurrent.RecursiveTask;
+
+public class FactorialTask extends RecursiveTask<Integer> {
+
+    int n;
+    int start;
+    int end;
+    final Integer THRESHOLD = 5;
+
+    public FactorialTask(Integer n) {
+        this.n = n;
+        this.start = 1;
+        this.end = n;
+    }
+
+    public FactorialTask(int start, int end) {
+        this.start = start;
+        this.end = end;
+        this.n = end - start + 1;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (n <= THRESHOLD) {
+            int result = 1;
+            for (int i = start; i <= end; i++) {
+                result *= i;
+            }
+            return result;
+        } else  {
+            int mid = (start + end) / 2;
+            FactorialTask f1 = new FactorialTask(start, mid);
+            FactorialTask f2 = new FactorialTask(mid+1, end);
+            f1.fork();
+            f2.fork();
+            return f1.join() * f2.join();
+        }
+    }
+}
